a805cc38d490f0ca816dffad27ad344725412501
小飞hub <2678199009@qq.com>
2017-11-12 16:01:23 +0800

    Initial Commit

---
+++ OCRDetector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCRDetector
+//
+//  Created by 王腾飞 on 2017/11/12.
+//  Copyright © 2017年 王腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
