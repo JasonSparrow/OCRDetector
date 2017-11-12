79742cf4f4108de4957a889d69abf854b8f8fc43
小飞hub <2678199009@qq.com>
2017-11-12 16:28:02 +0800

    add opencv

---
+++ OCRDetector/ProcessImage.h
@@ -0,0 +1,13 @@
+//
+//  ProcessImage.h
+//  OCR
+//
+//  Created by 王腾飞 on 2017/11/9.
+//  Copyright © 2017年 王腾飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProcessImage : NSObject
+
+@end
---
+++ OCRDetector/ProcessImage.mm
@@ -0,0 +1,16 @@
+//
+//  ProcessImage.m
+//  OCR
+//
+//  Created by 王腾飞 on 2017/11/9.
+//  Copyright © 2017年 王腾飞. All rights reserved.
+//
+
+#import "ProcessImage.h"
+#import <opencv2/opencv.hpp>
+//#import <opencv2/highgui/cap_ios.h>
+#import <opencv2/highgui/highgui_c.h>
+
+@implementation ProcessImage
+
+@end
